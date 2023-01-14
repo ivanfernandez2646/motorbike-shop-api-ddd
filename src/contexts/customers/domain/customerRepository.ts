2dc709ec377504c0dc2cf715b6582b9e095fffad
Iván <ivanfsdeveloper@gmail.com>
2023-01-14 11:40:00 +0100

    chore: mongoRepository interface (domain)

---
+++ src/contexts/customers/domain/customerRepository.ts
@@ -0,0 +1,11 @@
+import { Nullable } from '../../shared/domain/nullable';
+import Customer from './customer';
+import CustomerId from './customerId';
+
+export interface CustomerRepository {
+  save(customer: Customer): Promise<void>;
+
+  search(id: CustomerId): Promise<Nullable<Customer>>;
+
+  delete(customemr: Customer): Promise<void>;
+}
