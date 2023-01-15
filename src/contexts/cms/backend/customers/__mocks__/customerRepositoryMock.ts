6961805852f9c450c568dc201d53fbe934d1e215
Iván <ivanfsdeveloper@gmail.com>
2023-01-15 18:40:19 +0100

    chore: customer repository mock

---
+++ src/contexts/cms/backend/customers/__mocks__/customerRepositoryMock.ts
@@ -0,0 +1,50 @@
+import Customer from '../domain/customer';
+import { CustomerRepository } from '../domain/customerRepository';
+import CustomerId from '../domain/customerId';
+import { Nullable } from '../../../../shared/domain/nullable';
+
+export default class CustomerRepositoryMock implements CustomerRepository {
+  private mockSave = jest.fn();
+
+  private mockSearch = jest.fn();
+
+  private mockDelete = jest.fn();
+
+  async save(customer: Customer): Promise<void> {
+    this.mockSave(customer);
+  }
+
+  assertSaveHasBeenCalledWith(customer: Customer): void {
+    const { mock } = this.mockSave,
+      lastSavedCustomer = mock.calls[mock.calls.length - 1][0] as Customer,
+      expectedBody = customer.toPrimitives(),
+      lastSavedCustomerBody = lastSavedCustomer.toPrimitives();
+
+    expect(lastSavedCustomer).toBeInstanceOf(Customer);
+    expect(expectedBody).toStrictEqual(lastSavedCustomerBody);
+  }
+
+  async search(id: CustomerId): Promise<Nullable<Customer>> {
+    return this.mockSearch(id);
+  }
+
+  whenSearchThenReturn(customer: Nullable<Customer>): void {
+    this.mockSearch.mockReturnValue(customer);
+  }
+
+  assertSearchHasBeenCalledWith(id: CustomerId): void {
+    expect(this.mockSearch).toHaveBeenLastCalledWith(id);
+  }
+
+  async delete(customer: Customer): Promise<void> {
+    this.mockDelete(customer);
+  }
+
+  assertDeleteHasBeenCalledWith(id: CustomerId): void {
+    expect(this.mockDelete).toHaveBeenLastCalledWith(id);
+  }
+
+  assertNotingDelete(): void {
+    expect(this.mockDelete).not.toHaveBeenCalled();
+  }
+}
