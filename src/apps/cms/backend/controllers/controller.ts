1aef849b4bf396d52f5ee59f7ec1ff5106ee2e97
Iván <ivanfsdeveloper@gmail.com>
2023-01-16 14:03:48 +0100

    chore: handle controller exceptions

---
+++ src/apps/cms/backend/controllers/controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+
+export type CustomException = { clazz: Function; statusCode: number };
+
+export default abstract class Controller {
+  async run(req: Request, res: Response) {
+    try {
+      await this._run(req, res);
+    } catch (err) {
+      if (err instanceof Error) {
+        this.handleError(err, res);
+      } else {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err);
+      }
+    }
+  }
+
+  private handleError(err: Error, res: Response) {
+    let isExceptionHandled: boolean = false;
+
+    for (const exception of this.exceptions()) {
+      if (err instanceof exception.clazz) {
+        res.status(exception.statusCode).send({ error: err.message });
+        isExceptionHandled = true;
+        break;
+      }
+    }
+
+    if (!isExceptionHandled) {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err);
+    }
+  }
+
+  protected exceptions(): CustomException[] {
+    return [];
+  }
+
+  protected abstract _run(req: Request, res: Response): Promise<void>;
+}
--- src/apps/cms/backend/controllers/customers/customerGetController.ts
+++ src/apps/cms/backend/controllers/customers/customerGetController.ts
@@ -1,17 +1,24 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-import { Controller } from '../Controller';
 import CustomerFinder from '../../../../../contexts/cms/backend/customers/application/find/customerFinder';
 import CustomerId from '../../../../../contexts/cms/backend/customers/domain/customerId';
+import Controller, { CustomException } from '../controller';
+import CustomerNotFound from '../../../../../contexts/cms/backend/customers/domain/customerNotFound';
 
-export default class CustomerGetController implements Controller {
+export default class CustomerGetController extends Controller {
   private readonly handler: CustomerFinder;
 
   constructor(handler: CustomerFinder) {
+    super();
+
     this.handler = handler;
   }
 
-  async run(req: Request, res: Response) {
+  protected exceptions(): CustomException[] {
+    return [{ clazz: CustomerNotFound, statusCode: 404 }];
+  }
+
+  async _run(req: Request, res: Response): Promise<void> {
     const id = new CustomerId(req.params.id);
     const customer = await this.handler.run(id);
 
