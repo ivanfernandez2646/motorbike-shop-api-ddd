8b85d91c59414e3dc6b4cb439014cf90746c12ef
Iván <ivanfsdeveloper@gmail.com>
2023-01-17 11:57:26 +0100

    tests: unit test jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   cacheDirectory: '.tmp/jestCache',
+  testMatch: ['**/*.test.ts'],
   testPathIgnorePatterns: ['<rootDir>/(.*)/persistence/(.*)']
 };
